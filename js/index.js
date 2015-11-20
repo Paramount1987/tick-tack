6c90efde2f93f145da46b85317b336fa235f50e0
Paramount2009 <Paramount7891@gmail.com>
2015-11-20 18:22:20 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+
+
+// new Vivus('game', {duration: 200, file: 'images/cross.svg'});
+
+	
+
+
+$(".field li").click(function(){
+
+	var id = $(this).attr("id");
+	
+	var cross = new Vivus(id, {type: 'oneByOne',duration: 50,file: "images/cross.svg"});
+
+})
+
+
+});
+
+
+
